671f48445d0bc3676b99650ae7a39aa33e9fb77c
George <gwmkwan@gmail.com>
2023-02-26 11:40:38 -0700

    style: add text styling and spacing

--- pages/work.vue
+++ pages/work.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="bg-black h-screen px-6 py-12">
     <h2 class="text-white font-audiowide text-5xl">MY WORK</h2>
-    <div class="flex">
-      <div v-for="n in 6">
+    <div class="flex space-x-2">
+      <div v-for="n in 6" class="">
         <img
           src="~/assets/mock-1.jpg"
           alt="project one"
           style="height: 70vh"
           class="object-cover"
         />
+        <p class="text-white font-space-mono text-sm">Project One</p>
       </div>
     </div>
   </div>
