345ea2be5e59f13eb218ab2de1d6534e57ff67c3
George <gwmkwan@gmail.com>
2023-02-26 11:05:18 -0700

    feat: setup canvas

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,42 @@
 <template>
-  <div
-    id="container"
-    class="absolute text-white text-center w-full max-w-2xl px-6"
-    style="
-      top: 50%;
-      transform: translate(-50%, -50%);
-      left: 50%;
-      width: 100;
-      color: #fff;
-      /* text-shadow: 0 0 0.5px #fff, 0 0 1px #fff, 0 0 1.5px #fff, 0 0 3px #fec47f,
+  <div>
+    <canvas ref="canvas"></canvas>
+    <div
+      id="container"
+      class="absolute text-white text-center w-full max-w-2xl px-6"
+      style="
+        top: 50%;
+        transform: translate(-50%, -50%);
+        left: 50%;
+        width: 100;
+        color: #fff;
+        /* text-shadow: 0 0 0.5px #fff, 0 0 1px #fff, 0 0 1.5px #fff, 0 0 3px #fec47f,
         0 0 3px #fec47f, 0 0 3px #fec47f, 0 0 3px #fec47f, 0 0 5px #fec47f; */
-    "
-  >
-    <h1
-      id="georgekwan"
-      class="font-Audiowide text-sm uppercase tracking-wide opacity-0"
-      style="transform: translateY(30px)"
+      "
     >
-      George Kwan
-    </h1>
-    <p
-      id="aboutMe"
-      class="font-Play text-4xl opacity-0"
-      style="transform: translateY(30px)"
-    >
-      EMBRACE OPPORTUNITIES, AND FOLLOW CURIOSITY.
-    </p>
-    <a
-      id="viewWorkBtn"
-      href="https://github.com/georgekwan"
-      class="border px-4 py-2 rounded-lg text-sm font-Audiowide uppercase mt-8 hover:bg-white hover:text-gray-800 inline-block opacity-0"
-      style="transform: translateY(30px)"
-    >
-      View Work
-    </a>
+      <h1
+        id="georgekwan"
+        class="font-Audiowide text-sm uppercase tracking-wide opacity-0"
+        style="transform: translateY(30px)"
+      >
+        George Kwan
+      </h1>
+      <p
+        id="aboutMe"
+        class="font-Play text-4xl opacity-0"
+        style="transform: translateY(30px)"
+      >
+        EMBRACE OPPORTUNITIES, AND FOLLOW CURIOSITY.
+      </p>
+      <a
+        id="viewWorkBtn"
+        href="https://github.com/georgekwan"
+        class="border px-4 py-2 rounded-lg text-sm font-Audiowide uppercase mt-8 hover:bg-white hover:text-gray-800 inline-block opacity-0"
+        style="transform: translateY(30px)"
+      >
+        View Work
+      </a>
+    </div>
   </div>
 </template>
 
@@ -108,11 +111,13 @@ export default {
       0.1,
       1000
     );
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({
+      canvas: this.$refs.canvas,
+    });
 
     renderer.setSize(innerWidth, innerHeight);
     renderer.setPixelRatio(devicePixelRatio);
-    document.body.appendChild(renderer.domElement);
+    // document.body.appendChild(renderer.domElement);
 
     new OrbitControls(camera, renderer.domElement);
     camera.position.z = 50;
