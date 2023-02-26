9bcb3c60c0760a4063633c0a912f1a1b896b19bc
George <gwmkwan@gmail.com>
2023-02-26 11:18:59 -0700

    style: add spacing

--- pages/work.vue
+++ pages/work.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-black h-screen">
-    <h2 class="text-white">WORK</h2>
+  <div class="bg-black h-screen px-6 py-12">
+    <h2 class="text-white font-audiowide text-5xl">MY WORK</h2>
   </div>
 </template>
