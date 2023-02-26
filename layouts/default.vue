ee09fc0cd825296739aee02b1419a20753ff9e4f
George <gwmkwan@gmail.com>
2023-02-26 10:32:11 -0700

    feat: create global file

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>***Testing Testing***</h1>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.font-Audiowide {
+  font-family: 'Audiowide', cursive;
+}
+.font-Play {
+  font-family: 'Play', sans-serif;
+  animation: pulsate 0.91s ease-in-out infinite alternate;
+}
+</style>
